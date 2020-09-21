fc090aa4c1d735f1e9b751140c2acfbc6458f27a
higebobo <higebobo@gmail.com>
2020-09-21 11:31:41 +0900

    modified

--- README.md
+++ README.md
@@ -34,4 +34,5 @@ For detailed explanation on how things work, check out [Nuxt.js docs](https://nu
 
 * [ContentfulからAPIを取得してNuxt\.jsで記事一覧を表示する \- 独学プログラマ](https://blog.cloud-acct.com/posts/blog-contentful-api)
 * [Nuxt\.jsにContentfulのブログ記事を表示する \- 独学プログラマ](https://blog.cloud-acct.com/posts/blog-nuxt-slugpage)
+* [Nuxt\.jsのgenerateプロパティに動的なルーティングを追加する \- 独学プログラマ](https://blog.cloud-acct.com/posts/blog-nuxtjs-generate-routes)
 * [【Nuxt\.js】ContentfulのMarkdown形式のブログコンテンツを表示する \| エンジニアの教科書](https://developers-book.com/2020/07/14/113/)
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 require('dotenv').config() // 追記
+const client = require('./plugins/contentful').default
 
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
@@ -54,5 +55,20 @@ export default {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN
+  },
+generate: {
+    routes() {
+      return Promise.all([
+        client.getEntries({
+          content_type: process.env.CTF_BLOG_POST_TYPE_ID
+        })
+      ]).then(([ posts ]) => {
+        return [
+          ...posts.items.map(post => {
+            return { route: `posts/${post.fields.slug}`, payload: post }
+          })
+        ]
+      })
+    }
   }
 }
